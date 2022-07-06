e8dcf5958fab287b0610b7ba73b09c8398019977
Jeffry Gonzalez <jeff@hypertheory.com>
2022-07-06 18:23:17 -0400

    prepared course api

--- backend/courses-api/Controllers/CoursesController.cs
+++ backend/courses-api/Controllers/CoursesController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using CoursesApi.Adapters;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
@@ -29,6 +30,29 @@ public class CoursesController : ControllerBase
         var data = await _adapter.GetCoursesCollection().Find(_ => true).Project(projection).ToListAsync();
         return Ok(new { data = data });
     }
+
+    [HttpPost()]
+    public async Task<ActionResult> CreateCourse([FromBody] CourseCreateRequest course)
+    {
+        var courseToCreate = new Course
+        {
+            Title = course.Title,
+            Description = course.Description,
+            NumberOfDays = course.NumberOfDays
+        };
+
+        await _adapter.GetCoursesCollection().InsertOneAsync(courseToCreate);
+
+        var response = new CourseListItem
+        {
+            Id = courseToCreate.Id.ToString(),
+            Title = courseToCreate.Title,
+            Description = courseToCreate.Description,
+            NumberOfDays = courseToCreate.NumberOfDays
+        };
+        return StatusCode(201, response);
+
+    }
 }
 
 public record CourseListItem
@@ -36,5 +60,15 @@ public record CourseListItem
     public string Id { get; init; } = string.Empty;
     public string Title { get; init; } = string.Empty;
     public string Description { get; init; } = string.Empty;
+    public int NumberOfDays { get; init; }
+}
+
+public record CourseCreateRequest
+{
+    [Required]
+    public string Title { get; init; } = string.Empty;
+    [Required]
+    public string Description { get; init; } = string.Empty;
+    [Range(1, 14)]
     public int NumberOfDays { get; init; }
 }
